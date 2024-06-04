d5fba59109d1776b934ccfa3c411eedb8265749f
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-04 20:57:12 +0900

    NextJS: Metadata

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,13 @@
+import { Metadata } from "next";
 import Navigation from "../components/navigation";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+};
 
 export default function NotFound() {
   return (
     <div>
-      <Navigation />
       <h1>Not found!</h1>
     </div>
   );
