9d325412ab9120d8f90c67fbef75d3adcfce2d4e
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-05 21:03:18 +0900

    NextJS: Data Fetching(Client Side Fetching, The way of React)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,20 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  });
+  return <div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>;
 }
