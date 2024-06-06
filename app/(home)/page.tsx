6baf6c45979ad0baa3fe8756bd64984b7131b0a9
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-06 21:28:10 +0900

    NextJS: Movie Styles

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
-import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
   // return fetch(API_URL).then((response) => response.json());
   const response = await fetch(API_URL);
   const json = await response.json();
@@ -18,11 +19,14 @@ async function getMovies() {
 export default async function HomePage() {
   const movies = await getMovies();
   return (
-    <div>
+    <div className={styles.container}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-        </li>
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        />
       ))}
     </div>
   );
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,15 +1,15 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  console.log(`Fetching movies: ${Date.now()}`);
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  throw new Error("Something is broken...");
-  //   const response = await fetch(`${API_URL}/${id}/videos`);
-  //   return response.json();
+  // console.log(`Fetching movies: ${Date.now()}`);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("Something is broken...");
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
-  console.log(`Finish getVideos: ${Date.now()}`);
+  // console.log(`Finish getVideos: ${Date.now()}`);
   return <h6>{JSON.stringify(videos)}</h6>;
 }
