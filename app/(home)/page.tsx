cb279ee78e4c0022bf0a56a469970a4cd67eab94
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-06 17:22:18 +0900

    NextJS: Data Fetching(Sequential Fetching(Requests))

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,29 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
-  // return fetch(URL).then((response) => response.json());
-  const response = await fetch(URL);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  // return fetch(API_URL).then((response) => response.json());
+  const response = await fetch(API_URL);
   const json = await response.json();
   return json;
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,30 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  //   console.log(props);
-  return <h1>Movie #{id}</h1>;
+  // console.log(props); // url parameters를 받을 수 있음(id, query)
+  console.log(`Start Fetching: ${Date.now()}`);
+  // getMovie Fetching을 끝낸 후 getVideos Fetching을 순차적으로 진행함
+  // 응답대기시간이 길어지게 됨
+  const movie = await getMovie(id);
+  const videos = await getVideos(id);
+  console.log(`End Fetching: ${Date.now()}`);
+  return <h1>{movie.title}</h1>;
 }
