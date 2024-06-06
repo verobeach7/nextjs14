6aafa1900040d4e2beb866efaea5337bd36a1750
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2024-06-06 20:57:44 +0900

    NextJS: CSS(Global styles, .module.css)

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,14 +2,13 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
-  const [count, setCount] = useState(0);
   return (
-    <nav>
-      <ul>
+    <nav className={styles.nav}>
+      <ul className={styles.list}>
         <li>
           <Link href="/">Home</Link>
           {path === "/" ? "🔥" : null}
@@ -17,10 +16,6 @@ export default function Navigation() {
         <li>
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" ? "🔥" : null}
-        </li>
-        <li>
-          {/* 개발자모드->Source->Run: Disable/Enable Javascript해보면 차이를 알게됨 */}
-          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
         </li>
       </ul>
     </nav>
